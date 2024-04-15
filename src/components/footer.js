c2c2626e7804febc6c64c3e466c8c6ad08888002
gold <aufe1995@gmail.com>
2024-04-15 16:28:53 +0900

    feat: add homepage default

---
+++ src/components/footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>Polar Bear</div>
+      <div>
+        <span>개발자 </span>: Alvin
+      </div>
+      <div>
+        <span>문 의 </span>: aufe1995@gmail.com
+      </div>
+      <div>
+        COPYRIGHT(C) <strong>github.com/aufe1995/Polar_Bear_frontend</strong>{" "}
+        ALL RIGHTS RESERVED.
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
